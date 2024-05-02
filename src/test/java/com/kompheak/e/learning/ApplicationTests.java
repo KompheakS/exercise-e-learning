0498d2e8ba3f644b9627425a3b699ed130e89c2b
Kompheak <kompheakkh10@gmail.com>
2024-05-02 21:42:12 +0700

    initial commit

---
+++ src/test/java/com/kompheak/e/learning/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kompheak.e.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
